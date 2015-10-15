9b228f906805cafffa2d473543b40594d9b77f9c
Joshua Cox <joshua.n.cox@gmail.com>
2015-10-14 20:35:47 -0700

    build process improved
    All directives, services, and controllers bundled into main.js with app
    and accessory script.js
    
    Uglifier added but not yet implemented

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,49 @@ gutil     = require('gulp-util'),
 webserver = require('gulp-webserver'),
 minifyCSS = require('gulp-minify-css'),
 rename    = require('gulp-rename'),
-sass      = require('gulp-sass');
+sass      = require('gulp-sass'),
+uglify    = require('gulp-uglify'),
+concat    = require('gulp-concat');
 
 gulp.task('js', function() {
-  gulp.src('builds/development/js/**/*')
+  gulp.src(['builds/development/js/**/*.js', 'builds/development/js/*.js'])
+    .pipe(concat('main.js'))
+    // .pipe(uglify())
+    .pipe(gulp.dest('builds/production'));  
 });
 
-gulp.task('html', function() {
-  gulp.src('builds/development/*.html')
+gulp.task('index', function() {
+  return gulp.src('builds/development/index.html')
+    .pipe(gulp.dest('builds/production'))
 });
 
-gulp.task('css', function() {
-  gulp.src('builds/development/css/*.css')
+gulp.task('views', function() {
+  return gulp.src('builds/development/views/*.html')
+    .pipe(gulp.dest('builds/production/views'))
 });
 
 gulp.task('sass', function() {
-  gulp.src('builds/development/components/sass/*.scss')
+  return gulp.src('builds/development/components/sass/*.scss')
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('builds/development/css'));
+    .pipe(gulp.dest('builds/production'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('builds/development/js/**/*', ['js']);
-  gulp.watch('builds/development/css/*.css', ['css']);
   gulp.watch(['builds/development/components/sass/*.scss',
     'builds/development/components/sass/pages/*.scss'], ['sass']);
-  gulp.watch(['builds/development/*.html',
-    'builds/development/views/*.html'], ['html']);
+  gulp.watch('builds/development/*.html', ['index']);
+  gulp.watch('builds/development/views/*.html', ['views']);
 });
 
 gulp.task('webserver', function() {
-  gulp.src('builds/development/')
+  gulp.src('builds/production/')
     .pipe(webserver({
       livereload: true,
       open: true
     }));
 });
 
-gulp.task('default', ['watch', 'html', 'js', 'css', 'sass', 'webserver']);
+gulp.task('default', ['watch', 'index', 'views', 'js', 'sass', 'webserver']);
